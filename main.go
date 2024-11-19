0c2f177b926f5913c8c56d06bce0325fc3059872
junstory <lastcom1@gmail.com>
2024-11-20 00:56:15 +0900

    Feat. User CRUD, Link user-rest

--- main.go
+++ main.go
@@ -26,16 +26,29 @@ func main() {
 	restaurantService := &services.RestaurantService{}
 	restaurantHandler := handlers.NewRestaurantHandler(restaurantService)
 
+	userService := &services.UserService{}
+	userHandler := handlers.NewUserHandler(userService)
+
 	//page
 	r.HandleFunc("/", handlers.IndexHandler)
 	r.HandleFunc("/restaurant-list", handlers.RestaurantListHandler)
 	r.HandleFunc("/restaurant/{id:[0-9]+}", handlers.RestaurantDetailHandler)
+
+	r.HandleFunc("/login", handlers.UserLoginHandler)
 
 	//api
 	r.HandleFunc("/api/restaurant-list", restaurantHandler.GetRestaurantList).Methods("GET")
 	r.HandleFunc("/api/restaurant/{id}", restaurantHandler.GetRestaurant).Methods("GET")
 	r.HandleFunc("/api/reservation", restaurantHandler.CreateReservation).Methods("POST")
 	r.HandleFunc("/api/reservation/{id}/status", restaurantHandler.UpdateReservationStatus).Methods("PUT")
+
+	r.HandleFunc("/api/login", userHandler.LoginUser).Methods("POST")
+	r.HandleFunc("/api/register", userHandler.CreateUser).Methods("POST")
+	r.HandleFunc("/api/user/{id}", userHandler.GetUser).Methods("GET")
+	r.HandleFunc("/api/user", userHandler.UpdateUser).Methods("PUT")
+	r.HandleFunc("/api/user/{id}", userHandler.DeleteUser).Methods("DELETE")
+
+	r.HandleFunc("/api/link", userHandler.LinkRestaurant).Methods("POST")
 
 	log.Println("Server starting at :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
--- models/user.go
+++ models/user.go
@@ -13,3 +13,8 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type LoginData struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
